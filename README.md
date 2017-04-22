e00ba70551ab3f434d45bd38ef5aec7c74fbba38
adamryvola <ryvolaadam@gmail.com>
2017-04-22 16:51:12 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# cloud-logger
+## Cloud-logger
+
+Send and save your logs in cloud.
+
+## Code Example
+
+```const logger = require('cloud-logger')('my-logger-server.com')```
+
+## Motivation
+
+Often all you need is simple flexible logger that saves everything in your own storage.
+
+## Installation
+
+```npm install cloud-logger -S```
+
+## API Reference
+
+console
+warn,warning
+error
+cloud
+
+## Tests
+
+```npm run test```
+
+## License
+
+GPL-3.0
---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class Logger {
+    constructor(connection='', options={}) {
+        this.connection = connection;
+        this.options = options;
+    }
+
+    console(data) {
+        console.log(data);
+    }
+
+    warn(data) {
+        console.warn(data);
+    }
+
+    warning(data) {
+        console.warn(data);
+    }
+
+    cloud(data) {
+        if (this.connection !== '') {
+            //TODO -> send to cloud
+        } else {
+            console.warn('Cloud logger -> no cloud connection');
+        }
+    }
+
+    requestMiddleware(req, res, next) {
+        console.log('Request-logger middleware');
+    }
+}
+
+const logger = new Logger();
+console.log(' =>> Cloud-logger initialized <<=');
+
+module.exports.logger = logger;
+module.exports = (connection, options) => {
+    logger.connection = connection;
+    logger.options = options;
+    return logger;
+};
