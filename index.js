f6365c82392e727805a27e652ea7fe6ebd44db31
adamryvola <ryvolaadam@gmail.com>
2017-05-20 14:42:19 +0200

    socket connection - full setup

--- index.js
+++ index.js
@@ -5,34 +5,58 @@ const io = require('socket.io-client');
 /**
  * Log levels
  *
- * 1 - cloud and console
- * 2 - cloud
+ * 1 - cloud
+ * 2 - cloud and console
  * 3 - console
  */
 
 class Logger {
 
-    constructor(connection = '', options = {}) {
-        this.socket = io('https://gile-log.herokuapp.com');
-        this.connection = connection;
+    constructor(connection, options = {}) {
+
+        this.callback = options.callback;
+        this.callbackTypes = options.callbackTypes;
         this.options = options;
-        if (!this.options.level) {
-            this.options.level = 3;
+
+        this.localOnly = false;
+        if (!connection) this.localOnly = true;
+
+        if (!this.localOnly && (!this.options.protocol || this.options.protocol !== 'http')) {
+            this.options.protocol = 'ws';
+            this.socket = io(connection);
         }
+
+        this.connection = connection;
+
+        if (!this.options.level) {
+            if (this.connection) {
+                this.options.level = 1;
+            } else {
+                this.options.level = 3;
+            }
+        }
+        if (this.localOnly && this.options.level !== 3) {
+            this.options.level = 3;
+            console.warn('[cloud-logger] logger is initialized for local use only, level=3');
+        }
+        console.log('[ =>> Cloud-logger initialized <<= ]');
     }
 
     log(data) {
         switch (this.options.level) {
             case 1:
                 this.cloud(data);
-                this.console(data);
                 break;
             case 2:
                 this.cloud(data);
+                this.console(data);
                 break;
             case 3:
                 this.console(data);
                 break;
+        }
+        if (this.callback && data.type && this.callbackTypes && this.callbackTypes.indexOf(data.type) > -1) {
+            this.callback(data);
         }
     }
 
@@ -49,14 +73,15 @@ class Logger {
     }
 
     cloud(data) {
-        /*
-        if (this.connection !== '') {
-            request.post({url: this.connection, json: true, body: data});
+        if (this.options.protocol === 'http') {
+            if (this.connection !== '') {
+                request.post({url: this.connection, json: true, body: data});
+            } else {
+                console.warn('Cloud logger -> no cloud connection for http post request');
+            }
         } else {
-            console.warn('Cloud logger -> no cloud connection');
+            this.socket.emit('POST_LOG', data);
         }
-        */
-        this.socket.emit('POST_LOG', {client: 'zadek', message: 'muj prvni log'});
     }
 
     requestMiddleware(req, res, next) {
@@ -72,7 +97,6 @@ class Logger {
 }
 
 const logger = new Logger();
-console.log(' =>> Cloud-logger initialized <<=');
 
 module.exports = (connection, options) => {
     logger.connection = connection;
